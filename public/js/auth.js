d24960f1b72d1a98fd9e1fe49b45283d0de8eecc
Wanga Kanjala <wangakanjala@gmail.com>
2026-06-03 11:27:48 +0200

    added authentication

---
+++ public/js/auth.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", event=> {
+  // Initialize Firebase
+  const app = firebase.app();});
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            const user = result.user;
+            // console.log("User Info:", user);
+            // Redirect to the dashboard after successful login
+            window.location.href = "dashboard.html";
+        })
+        .catch(error => {
+            console.error("Error during Google login:", error);
+            alert("Failed to log in with Google. Please try again.");
+        });
+} 
